09fa26cdee5ed26705a403aa250939a3d8186560
luutry <luutry@163.com>
2020-10-27 10:45:25 +0800

    mall 商城技术业务学习项目 开始

---
+++ src/main/java/com/luutry/malldetail/MallDetailApplication.java
@@ -0,0 +1,13 @@
+package com.luutry.malldetail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallDetailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallDetailApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/luutry/malldetail/MallDetailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luutry.malldetail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallDetailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
